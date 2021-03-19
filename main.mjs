f10fdac64d03e1aad2afed61f44b87a2dc49c9db
instantjohn <sensiblejohn@hotmail.com>
2021-03-19 21:05:40 +0900

    first commit

---
+++ main.mjs
@@ -0,0 +1,28 @@
+
+console.log("hello");
+
+import sdk from 'matrix-js-sdk';
+
+const client = sdk.createClient("https://matrix.org");
+
+await client.login("m.login.password", {
+  "user": "", // <- ここにパスワード
+  "password": "" // <- ここにユーザーネーム
+}).then((response) => {
+    console.log(response.access_token);
+});
+
+var testRoomId = ""; // room id
+
+var content = {
+    "body": "Hello world"
+    "msgtype": "m.text"
+};
+
+client.sendEvent(testRoomId, "m.room.message", content, "").then((res) => {
+  // message sent successfully
+  console.log("message sent successfully");
+}).catch((err) => {
+    console.log(err);
+})
+
